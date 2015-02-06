63e04ba356c3608af69c98c2927eff240c560e06
cleveranjos <clever@clever.com.br>
2015-02-06 16:00:37 -0200

    Fix
    Fixing the issue when two objects are added to same screen, properties
    were been overlaped

--- extension/br.com.clever.wordcloud.js
+++ extension/br.com.clever.wordcloud.js
@@ -14,146 +14,143 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 */
 /*global define , window, Qv, jQuery, d3, $, document  */
 define(["jquery", "./br.com.clever.wordcloud.support", "text!./styles.css"], function ($) {
-	'use strict';
-	return {
-		initialProperties : {
-			version : 1.0,
-			qHyperCubeDef : {
-				qDimensions : [],
-				qMeasures : [],
-				qInitialDataFetch : [{
-					qWidth : 2,
-					qHeight : 100
-				}]
-			}
-		},
-		//property panel
-		definition : {
-			type : "items",
-			component : "accordion",
-			items : {
-				dimensions : {
-					uses : "dimensions",
-					min : 1,
-					max : 1
-				},
-				measures : {
-					uses : "measures",
-					min : 1,
-					max : 1
-				},
-				sorting : {
-					uses : "sorting"
-				},
-				addons : {
-					uses : "addons",
-					items : {
-						Orientations : {
-							ref : "Orientations",
-							label : "Orientations",
-							type : "number",
-							defaultValue : 2,
-							min : 2,
-							max : 10
-						},
-						RadStart : {
-							ref : "RadStart",
-							label : "Start Angle",
-							type : "number",
-							defaultValue : -90,
-							min : -90,
-							max : 90
-						},
-						RadEnd : {
-							ref : "RadEnd",
-							label : "End Angle",
-							type : "number",
-							defaultValue : 90,
-							min : -90,
-							max : 90
-						},
-						MaxSize : {
-							ref : "MaxSize",
-							label : "Font Max Size",
-							type : "integer",
-							defaultValue : 100,
-							min : 10,
-							max : 200
-						},
-						MinSize : {
-							ref : "MinSize",
-							label : "Font Min Size",
-							type : "integer",
-							defaultValue : 20,
-							min : 10,
-							max : 200
-						},
-						Scale : {
-							type : "string",
-							component : "dropdown",
-							label : "Scale",
-							ref : "Scale",
-							options : [{
-								value : "log",
-								label : "Log"
-							}, {
-								value : "linear",
-								label : "Linear"
-							}],
-							defaultValue : "linear"
-						},
-						ScaleColor : {
-							type : "string",
-							component : "dropdown",
-							label : "Scale Color",
-							ref : "ScaleColor",
-							options : [{
-								value : "category10",
-								label : "category10"
-							}, {
-								value : "category20",
-								label : "category20"
-							}, {
-								value : "category20b",
-								label : "category20b"
-							}, {
-								value : "category20c",
-								label : "category20c"
-							}],
-							defaultValue : "category20"
-						}
-					}
-				},
-				settings : {
-					uses : "settings"
-				}
-			}
-		},
-		snapshot : {
-			canTakeSnapshot : true
-		},
-		paint : function ($element, layout) {
-			if (layout.qHyperCube.qDimensionInfo && layout.qHyperCube.qDimensionInfo.length > 0) {
-				var id = "wordcloud_" + layout.qInfo.qId,
-					words = layout.qHyperCube.qDataPages[0].qMatrix.map(function (row) {
-						return {
-							text : row[0].qText,
-							value : row[1].qText
-						};
-					}),
-					cloud = d3.wordcloud().id(id)
-								.width($element.width())
-								.height($element.height());
-				if (document.getElementById(id)) {
-					$("#" + id).empty();
-					cloud(words, layout);
-				} else {
-					$element.append($('<div />')
-						.attr("id", id)
-						.width($element.width())
-						.height($element.height()));
-				}
-			}
-		}
-	};
+    'use strict';
+    return {
+        initialProperties : {
+            version : 1.0,
+            qHyperCubeDef : {
+                qDimensions : [],
+                qMeasures : [],
+                qInitialDataFetch : [{
+                    qWidth : 2,
+                    qHeight : 100
+                }]
+            }
+        },
+        //property panel
+        definition : {
+            type : "items",
+            component : "accordion",
+            items : {
+                dimensions : {
+                    uses : "dimensions",
+                    min : 1,
+                    max : 1
+                },
+                measures : {
+                    uses : "measures",
+                    min : 1,
+                    max : 1
+                },
+                sorting : {
+                    uses : "sorting"
+                },
+                addons : {
+                    uses : "addons",
+                    items : {
+                        Orientations : {
+                            ref : "Orientations",
+                            label : "Orientations",
+                            type : "number",
+                            defaultValue : 2,
+                            min : 2,
+                            max : 10
+                        },
+                        RadStart : {
+                            ref : "RadStart",
+                            label : "Start Angle",
+                            type : "number",
+                            defaultValue : -90,
+                            min : -90,
+                            max : 90
+                        },
+                        RadEnd : {
+                            ref : "RadEnd",
+                            label : "End Angle",
+                            type : "number",
+                            defaultValue : 90,
+                            min : -90,
+                            max : 90
+                        },
+                        MaxSize : {
+                            ref : "MaxSize",
+                            label : "Font Max Size",
+                            type : "integer",
+                            defaultValue : 100,
+                            min : 10,
+                            max : 200
+                        },
+                        MinSize : {
+                            ref : "MinSize",
+                            label : "Font Min Size",
+                            type : "integer",
+                            defaultValue : 20,
+                            min : 10,
+                            max : 200
+                        },
+                        Scale : {
+                            type : "string",
+                            component : "dropdown",
+                            label : "Scale",
+                            ref : "Scale",
+                            options : [{
+                                value : "log",
+                                label : "Log"
+                            }, {
+                                value : "linear",
+                                label : "Linear"
+                            }],
+                            defaultValue : "linear"
+                        },
+                        ScaleColor : {
+                            type : "string",
+                            component : "dropdown",
+                            label : "Scale Color",
+                            ref : "ScaleColor",
+                            options : [{
+                                value : "category10",
+                                label : "category10"
+                            }, {
+                                value : "category20",
+                                label : "category20"
+                            }, {
+                                value : "category20b",
+                                label : "category20b"
+                            }, {
+                                value : "category20c",
+                                label : "category20c"
+                            }],
+                            defaultValue : "category20"
+                        }
+                    }
+                },
+                settings : {
+                    uses : "settings"
+                }
+            }
+        },
+        snapshot : {
+            canTakeSnapshot : true
+        },
+        paint : function ($element, layout) {
+            if (layout.qHyperCube.qDimensionInfo && layout.qHyperCube.qDimensionInfo.length > 0) {
+                var id = "wordcloud_" + layout.qInfo.qId,
+                    words = layout.qHyperCube.qDataPages[0].qMatrix.map(function (row) {
+                        return {
+                            text : row[0].qText,
+                            value : row[1].qText
+                        };
+                    }),
+                    cloud = d3.wordcloud.id(id).width($element.width()).height($element.height());
+                if (document.getElementById(id)) {
+                    $("#" + id).empty();
+					cloud.go(words, layout);
+                } else {
+                    $element.append($('<div />').attr("id", id)
+                        .width($element.width())
+                        .height($element.height()));
+                }
+            }
+        }
+    };
 });
--- extension/br.com.clever.wordcloud.support.js
+++ extension/br.com.clever.wordcloud.support.js
@@ -15,68 +15,75 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 /*global define , window, Qv, jQuery, d3, $, document  */
 
 define(["jquery", "./d3.min", "./d3.layout.cloud"], function ($) {
-	d3.wordcloud = function () {
-		var id = '', width = 0, height = 0,
-			wordcloud = function (words, layout) {
-				fill = d3.scale[layout.ScaleColor]();
-				var max = layout.qHyperCube.qMeasureInfo[0].qMax,//words[0].value,
-					min = layout.qHyperCube.qMeasureInfo[0].qMin,// words.slice(-1)[0].value,
-					scale = d3.scale[layout.Scale]()
-								.domain([min, max])
-								.rangeRound([layout.MinSize, layout.MaxSize]), // Min size, max size
-					from = Math.max(-90, Math.min(90, +layout.RadStart)),
-					to = Math.max(-90, Math.min(90, +layout.RadEnd)),
-					scaleRotate = d3.scale.linear().domain([0, +layout.Orientations - 1]).range([from, to]);
-
-				d3.layout.cloud().size([width, height])
-					.words(words)
-					.padding(5)
-					.timeInterval(10)
-					.rotate(function (d, i) { return scaleRotate(i); })
-					.fontSize(function (d) { return scale(+d.value); })
-					.on("end", draw)
-					.start();
-				return wordcloud;
-			};
-		draw = function (words) {
-			var svg = d3.select("#" + id).append("svg")
-					.attr("width", width)
-					.attr("height", height)
+    d3.wordcloud = {
+        Id : '',
+        Width : 0,
+        Height : 0,
+        fill : null,
+		drawStub : function (words) {
+			var fill = d3.scale["layout.ScaleColor"](),				
+				svg = d3.select("#oId").append("svg")
+					.attr("width", "oWidth")
+					.attr("height", "oHeight")
 					.attr("class", "wordcloud")
 					.append("g")
-					.attr("transform", "translate(" + [width / 2, height / 2] + ")");
-			svg.selectAll("text")
-				.data(words)
-				.enter().append("text")
-				.style("fill", function (d, i) { return fill(i); })
-				.attr("text-anchor", "middle")
-				.attr("transform", function (d) { return "translate(" + [d.x, d.y] + ") rotate(" + d.rotate + ")"; })
-				.style("font-size", function (d) {	return d.size + "px"; })
-				.text(function (d) { return d.text; })
-				.append("svg:title").text(function (d) {return d.text + ':' + d.value; });
-			return wordcloud;
-		};
-		wordcloud.id = function (x) {
-			if (!arguments.length) {
-				return id;
-			}
-			id = x;
-			return wordcloud;
-		};
-		wordcloud.width = function (x) {
-			if (!arguments.length) {
-				return width;
-			}
-			width = x;
-			return wordcloud;
-		};
-		wordcloud.height = function (x) {
-			if (!arguments.length) {
-				return height;
-			}
-			height = x;
-			return wordcloud;
-		};
-		return wordcloud;
-	};
+					.attr("transform", "translate(" + ["oWidth" / 2, "oHeight" / 2] + ")");
+				svg.selectAll("text")
+					.data(words)
+					.enter().append("text")
+					.style("fill", function (d, i) { return fill(i); })
+					.attr("text-anchor", "middle")
+					.attr("transform", function (d) { return "translate(" + [d.x, d.y] + ") rotate(" + d.rotate + ")"; })
+					.style("font-size", function (d) { return d.size + "px"; })
+					.text(function (d) { return d.text;})
+					.append("svg:title").text(function (d) { return d.text + ':' + d.value; });
+		},  
+        go : function (words, layout) {
+            var max = layout.qHyperCube.qMeasureInfo[0].qMax,
+                min = layout.qHyperCube.qMeasureInfo[0].qMin,
+                scale = d3.scale[layout.Scale]()
+                        .domain([min, max])
+                        .rangeRound([layout.MinSize, layout.MaxSize]), // Min size, max size
+                from = Math.max(-90, Math.min(90, +layout.RadStart)),
+                to = Math.max(-90, Math.min(90, +layout.RadEnd)),
+                scaleRotate = d3.scale.linear().domain([0, +layout.Orientations - 1]).range([from, to]),
+				drawFunction = this.drawStub.toString() // bad way of keeping value of "this" when callingback from "d3"
+								.replace("layout.ScaleColor", layout.ScaleColor)
+								.replace(/oId/g, this.Id)
+								.replace(/"oWidth"/g, this.Width)
+								.replace(/"oHeight"/g, this.Height)
+								.replace(/layoutScaleColor/g, layout.ScaleColor);
+			
+            d3.layout.cloud().size([this.Width, this.Height])
+                .words(words)
+                .padding(5)
+                .timeInterval(10)
+                .rotate(function (d, i) { return scaleRotate(i); })
+                .fontSize(function (d) { return scale(+d.value); })
+                .on("end", eval ('(' + drawFunction + ')'))
+				.start();
+            return this;
+        },
+        id : function (x) {
+            if (!arguments.length) {
+                return this.Id;
+            }
+            this.Id = x;
+            return this;
+        },
+        width : function (x) {
+            if (!arguments.length) {
+                return this.Width;
+            }
+            this.Width = x;
+            return this;
+        },
+        height : function (x) {
+            if (!arguments.length) {
+                return this.Height;
+            }
+            this.Height = x;
+            return this;
+        }
+    };
 });
