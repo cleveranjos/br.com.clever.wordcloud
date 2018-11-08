bcbe94863bf60b69e24e5ee93468e92d95995128
Alexander Szmidt <alexander.szmidt@gmail.com>
2018-11-08 11:34:40 +0100

    [QPE-225] Replace d3.layout.cloud.js with npm package d3-cloud

--- src/qlik-word-cloud.js
+++ src/qlik-word-cloud.js
@@ -14,7 +14,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 
 /* global define, window, d3, document, layout, $  */
 
-import d3cloud from './d3.layout.cloud.js';
+import d3cloud from 'd3-cloud';
 import './styles.css';
 
 var self;
